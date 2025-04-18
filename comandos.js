4809896ea2b134c2c199d2ca9702d86a6d5197d2
JoaoPauloHJ <joaopaulohj126@hotmail.com>
2025-04-18 19:21:36 -0300

    Contador att

--- comandos.js
+++ comandos.js
@@ -1,6 +1,6 @@
 function atualizarContador() {
     // Defina a data final (Ano, Mês (0-11), Dia, Hora, Minuto, Segundo)
-    var dataFinal = new Date(2025, 2, 19, 17, 0, 0); // 20 de março de 2025 às 23:59:59
+    var dataFinal = new Date(2025, 9, 20, 17, 0, 0); // 20 de março de 2025 às 23:59:59
 
     var agora = new Date();
     var diferenca = dataFinal - agora; // Diferença em milissegundos
