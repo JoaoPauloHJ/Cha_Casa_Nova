1cf4afe8fae551cb4aa63d2b4fa6df6d9b122b8b
JoaoPauloHJ <joaopaulohj126@hotmail.com>
2025-05-01 17:26:32 -0300

    Att fotos

--- comandos.js
+++ comandos.js
@@ -297,14 +297,14 @@ const produtos = [
     {
         nome: "Espremedor de Suco",
         valor: "R$ 140,00",
-        imagem: "img/espremedorDeSuco.png",
+        imagem: "img/espremedordeSuco.png",
         pagamento: "https://checkout.nubank.com.br/DiNnlzaRiG92botq",
         whatsapp: "https://wa.me/5534996920066/?text=Ol%C3%A1,%20Gostaria%20de%20reservar%20esse%20presente%20(Espremedor%20De%Suco).",
     },
     {
         nome: "Boleira",
         valor: "R$ 70,00",
-        imagem: "img/boleira.png",
+        imagem: "img/boleiraa.png",
         pagamento: "https://checkout.nubank.com.br/DiNnlzaRiG92botq",
         whatsapp: "https://wa.me/5534996920066/?text=Ol%C3%A1,%20Gostaria%20de%20reservar%20esse%20presente%20(Boleira).",
     }
